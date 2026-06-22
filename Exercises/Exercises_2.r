703419cb77e027144ae1b87ab055c8707df79684
Hamidullah Rezae <hamidullahrezae15@gmail.com>
2026-06-23 08:44:57 +1000

    Simulate geometric distribution via repeated Binomial flips and verify with rgeom

--- Exercises/Exercises_2.r
+++ Exercises/Exercises_2.r
@@ -397,3 +397,20 @@ Y <- rpois(100000, 2)
 Z <- X + Y
 # Use compare_histograms to compare Z to the Poisson(3)
 compare_histograms(Z, rpois(100000, 3))
+
+#Simulate geometric distribution via 
+#repeated Binomial flips and verify with rgeom
+# Simulate 100 instances of flipping a 20% coin
+flips <- rbinom(100, 1, .2)
+# Use which to find the first case of 1 ("heads")
+which(flips == 1)[1]
+# Existing code for finding the first instance of heads
+which(rbinom(100, 1, 0.2) == 1)[1]
+# Replicate this 100,000 times using replicate()
+replications <- replicate(100000, which(rbinom(100, 1, 0.2) == 1)[1])
+# Histogram the replications with qplot
+qplot(replications)
+# Generate 100,000 draws from the corresponding geometric distribution
+geom_sample <- rgeom(100000, .2)
+# Compare the two distributions with compare_histograms
+compare_histograms(replications, geom_sample)
